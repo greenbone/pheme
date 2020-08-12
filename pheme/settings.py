ae13ee5d3e7b4eed9fa649136409e89ef81c7613
Philipp Eder <philipp.eder@greenbone.net>
2020-08-12 12:12:58 +0200

    add pylint-django

--- pheme/settings.py
+++ pheme/settings.py
@@ -73,29 +73,13 @@ WSGI_APPLICATION = 'pheme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
