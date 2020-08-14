e51f50d94616f5a9a6cd8544d6dd566de470aa66
Philipp Eder <philipp.eder@greenbone.net>
2020-08-14 18:43:11 +0200

    intoduce dataclass instead of dict to use it as template description

--- pheme/transformation/scanreport/gvmd.py
+++ pheme/transformation/scanreport/gvmd.py
@@ -1,46 +1,144 @@
+# pylint: disable=W0614,W0511,W0401,C0103
+import dataclasses
 from functools import reduce
-from typing import Callable, Optional
+from typing import Callable, Optional, List, Union, Tuple
+from .model import *
+
+
+def __find_first_occurance(
+    candidates: List[Union[HostResults, NVTResult]], is_the_chosen_one: Callable
+) -> Tuple[int, Optional[Union[HostResults, NVTResult]]]:
+    for i, r in enumerate(candidates):
+        if is_the_chosen_one(r):
+            return i, r
+    return -1, None
 
 
 def group_by_host(first, second):
     host = second.pop('host')
     host = host if isinstance(host, str) else host["text"]
-    to_add = [second]
-    previous = first.get(host)
-    if previous:
-        to_add += previous
-    first[host] = to_add
+    index, hr = __find_first_occurance(first, lambda h: h.host == host)
+    nvt = second.pop('nvt')
+
+    solution = (
+        Solution(nvt['solution']['type'], nvt['solution']['text'])
+        if nvt.get('solution')
+        else None
+    )
+    refs = (
+        [Ref(r['id'], r['type']) for r in nvt['refs']['ref']]
+        if nvt.get('refs')
+        else []
+    )
+    oid = nvt['oid']
+    shr = HostResult(
+        oid,
+        nvt['type'],
+        nvt['name'],
+        nvt['family'],
+        nvt['cvss_base'],
+        nvt['tags'],
+        solution,
+        refs,
+        second['port'],
+        second['threat'],
+        second['severity'],
+        QOD(second['qod']['value'], second['qod']['type'],),
+        second['description'],
+    )
+    if hr:
+        hr.results.append(shr)
+        first[index] = hr
+    else:
+        first.append(HostResults(host, [shr]))
     return first
 
 
 def group_by_nvt(first, second):
     nvt = second.pop('nvt')
     host = second.pop('host')
-    second.pop('owner', None)
     host = host if isinstance(host, str) else host["text"]
-    to_add = [{host: second}]
     oid = nvt['oid']
-    previous = first.get(oid)
-    to_add += previous['hosts'] if previous else []
-    nvt['hosts'] = to_add
-    first[oid] = nvt
+    index, nvt_result = __find_first_occurance(first, lambda n: n.oid == oid)
+    hs = HostSpecific(host, second.get('description'))
+    if nvt_result:
+        nvt_result.hosts.append(hs)
+        first[index] = nvt_result
+    else:
+        solution = (
+            Solution(nvt['solution']['type'], nvt['solution']['text'])
+            if nvt.get('solution')
+            else None
+        )
+        refs = (
+            [Ref(r['id'], r['type']) for r in nvt['refs']['ref']]
+            if nvt.get('refs')
+            else []
+        )
+        first.append(
+            NVTResult(
+                oid,
+                nvt['type'],
+                nvt['name'],
+                nvt['family'],
+                nvt['cvss_base'],
+                nvt['tags'],
+                solution,
+                refs,
+                second['port'],
+                second['threat'],
+                second['severity'],
+                QOD(second['qod']['value'], second['qod']['type'],),
+                [hs],
+            )
+        )
     return first
 
 
 def transform(data: dict, group_by: Optional[Callable] = group_by_host) -> dict:
-    # add flavours ...
     result = dict(data)
-    if group_by:
-        raw_results = result["report"]["report"]["results"]["result"]
-        grouped = reduce(group_by, raw_results, {})
-        result['report']['report']['results']['result'] = grouped
-    # delete detailed host information
-    result['report']['report'].pop('host', None)
-    result['report'] = result['report']['report']
-    # delete filter information
-    result['report'].pop('filters', None)
-    result['report'].pop('sort', None)
-    result['report'].pop('severity_class', None)
-    # already in result information
-    result['report'].pop('ports', None)
-    return result
+    report = result["report"]["report"]
+    raw_results = report["results"]["result"]
+    grouped = reduce(group_by, raw_results, [])
+
+    return dataclasses.asdict(
+        Report(
+            report['id'],
+            Version(**report['gmp']),
+            report['scan_run_status'],
+            Count(**report['hosts']),
+            Count(**report['closed_cves']),
+            Count(**report['vulns']),
+            Count(**report['os']),
+            Count(**report['apps']),
+            Count(**report['ssl_certs']),
+            Task(
+                report.get('task').get('id'),
+                report.get('task').get('name'),
+                report.get('task').get('comment'),
+                Target(**report.get('task').get('target')),
+                report.get('task').get('progress'),
+            ),
+            report.get('timestamp'),
+            report.get('scan_start'),
+            report.get('timezone'),
+            report.get('timezone_abbrev'),
+            report.get('scan_end'),
+            Count(**report['errors']),
+            Results(
+                report['results']['max'], report['results']['start'], grouped
+            ),
+            Filtered(**report['severity']),
+            ResultCount(
+                report['result_count']['full'],
+                report['result_count']['filtered'],
+                Filtered(**report['result_count']['debug']),
+                Filtered(**report['result_count']['hole']),
+                Filtered(**report['result_count']['info']),
+                Filtered(**report['result_count']['log']),
+                Filtered(**report['result_count']['warning']),
+                Filtered(**report['result_count']['false_positive']),
+                report['result_count']['text'],
+            ),
+        ),
+    )
--- pheme/transformation/scanreport/model.py
+++ pheme/transformation/scanreport/model.py
@@ -1,2 +1,144 @@
+# pylint: disable=W0614,W0511,W0401,C0103
+from typing import List, Union
+from dataclasses import dataclass
+
+
+@dataclass
+class Version:
+    version: str
+
+
+@dataclass
+class Count:
+    count: str
+
+
+@dataclass
+class Identifieable:
+    id: str
+    name: str
+    comment: str
+
+
+@dataclass
+class Target(Identifieable):
+    trash: str
+
+
+@dataclass
+class Task(Identifieable):
+    progress: str
+    target: Target
+
+
+@dataclass
+class Scan:
+    task: Task
+
+
+@dataclass
+class Filtered:
+    full: str
+    filtered: str
+
+
+@dataclass
+class ResultCount(Filtered):
+    debug: Filtered
+    hole: Filtered
+    info: Filtered
+    log: Filtered
+    warning: Filtered
+    false_positive: Filtered
+    text: str
+
+
+@dataclass
+class Solution:
+    type: str
+    text: str
+
+
+@dataclass
+class Ref:
+    id: str
+    type: str
+
+
+@dataclass
+class QOD:
+    value: str
+    type: str
+
+
+@dataclass
+class NVT:
+    oid: str
+    type: str
+    name: str
+    family: str
+    cvss_base: str
+    tags: str
+    solution: Solution
+    refs: List[Ref]
+
+
+@dataclass
+class HostSpecific:
+    name: str
+    description: str
+
+
+@dataclass
+class Result(NVT):
+    port: str
+    threat: str
+    severity: str
+    qod: QOD  # atm not in nvt
+
+
+@dataclass
+class NVTResult(Result):
+    hosts: List[HostSpecific]
+
+
+@dataclass
+class HostResult(Result):
+    description: str
+
+
+@dataclass
+class HostResults:
+    host: str
+    # TODO rename
+    results: List[HostResult]
+
+
+@dataclass
+class Results:
+    max: str
+    start: str
+    scans: List[Union[NVTResult, HostResults]]
+
+
+@dataclass
 class Report:
-    pass
+    id: str
+    gmp: Version
+    scan_run_status: str
+    hosts: Count
+    closed_cves: Count
+    vulns: Count
+    os: Count
+    apps: Count
+    ssl_certs: Count
+    task: Task
+    timestamp: str
+    scan_start: str
+    timezone: str
+    timezone_abbrev: str
+    scan_end: str
+    errors: Count
+    results: Results
+    severity: Filtered
+    result_count: ResultCount
