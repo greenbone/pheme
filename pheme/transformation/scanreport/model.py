1a26794e172fb4739d3077422560bf80e40394a0
Philipp Eder <philipp.eder@greenbone.net>
2020-08-18 10:37:57 +0200

    add license header

--- pheme/transformation/scanreport/model.py
+++ pheme/transformation/scanreport/model.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+# pheme/transformation/scanreport/model.py
+# Copyright (C) 2020 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # pylint: disable=W0614,W0511,W0401,C0103
 from typing import List, Union
 from dataclasses import dataclass
