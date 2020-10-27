5ea59181f2bcc7217ef1694c14ae62776f428012
Philipp Eder <philipp.eder@greenbone.net>
2020-10-27 10:41:20 +0100

    fix double image tag on content-type

--- pheme/datalink.py
+++ pheme/datalink.py
@@ -4,4 +4,4 @@ import base64
 
 def as_datalink(data: bytes, file_format: str) -> str:
     img = urllib.parse.quote(base64.b64encode(data))
-    return 'data:image/{};base64,{}'.format(file_format, img)
+    return 'data:{};base64,{}'.format(file_format, img)
